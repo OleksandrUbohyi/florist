2da2f22788d42991a6f30df0edc6c5845c18df9b
Zadorozhnyi Oleksandr <global2@mail.ua>
2021-02-08 15:34:21 +0200

    added tablet version

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ const swiper = new Swiper(".swiper-container", {
     // Optional parameters
     direction: "horizontal",
     loop: true,
+    speed: 1000,
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
@@ -11,21 +12,25 @@ const swiper = new Swiper(".swiper-container", {
     pagination: {
         el: ".swiper-pagination",
     },
+
+    breakpoints: {
+        // when window width is >= 768px
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 28,
+            slidesPerGroup: 2,
+        },
+    },
 });
 
 const headerHamburger = document.querySelector(".header__hamburger");
 headerHamburger.addEventListener("click", () => {
     document.getElementById("myNav").style.height = "100%";
+    document.body.style.overflowY = "hidden";
 });
 
 const overlayClose = document.querySelector(".overlay__close");
 overlayClose.addEventListener("click", () => {
     document.getElementById("myNav").style.height = "0%";
+    document.body.style.overflowY = "";
 });
-
-(function setFullheight() {
-    let vh = window.innerHeight * 0.01;
-    document.querySelector(".header").style.setProperty("--vh", `${vh}px`);
-})();
-
-window.addEventListener("resize", setFullheight);
